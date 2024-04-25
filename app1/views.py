35eb35bd3569761a82789fe0d2c16a9e9d3a3022
Pramila672 <pramilakhadka672@gmail.com>
2024-04-25 14:24:29 +0545

    .gitignore added

--- app1/views.py
+++ app1/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse , HttpResponseNotFound
-from django.views.decorators.http import require_http_methods , require_POST
+from django.views.decorators.http import require_http_methods ,require_safe
+from django.views.decorators.cache import cache_page
 
 # Create your views here.
+@cache_page(3600)
+@require_safe()
 #@require_http_methods(["POST"])
-@require_GET
 #@require_POST
+
 def home(request):
-    print("Home is called.")
+    print("Hey caching is working fine")
+    #print("Home is called.")
     return HttpResponse("<h1>This is my first view page</h1>" , status=500)
     #return HttpResponseNotFound("<h1>The Http request hasnot been found</h1>"
