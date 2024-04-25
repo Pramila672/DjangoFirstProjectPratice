959368509d307932e7cda9768646ebf2fbcc4205
Pramila672 <pramilakhadka672@gmail.com>
2024-04-25 13:42:24 +0545

    Decorators and HttpResponse concept

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse , HttpResponseNotFound
+from django.views.decorators.http import require_http_methods , require_POST
 
 # Create your views here.
+#@require_http_methods(["POST"])
+@require_GET
+#@require_POST
 def home(request):
     print("Home is called.")
-    return HttpResponse("<h1>This is my first view page</h1>")
+    return HttpResponse("<h1>This is my first view page</h1>" , status=500)
+    #return HttpResponseNotFound("<h1>The Http request hasnot been found</h1>"
