d2e6441cb78f24932f79f7a631a12149e5c6e665
Pramila672 <pramilakhadka672@gmail.com>
2024-04-29 12:08:07 +0545

    Redirecting to login view

--- app1/views.py
+++ app1/views.py
@@ -1,15 +1,22 @@
 from django.http import HttpResponse , HttpResponseNotFound
 from django.views.decorators.http import require_http_methods , require_safe
 from django.views.decorators.cache import cache_page
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render , redirect
 # Create your views here.
-@cache_page(3600)
+#@cache_page(3600)
 #@require_safe()
-#@require_http_methods(["POST"])
-#@require_POST
+#@require_http_methods(["GET"])
+@login_required
 
 def home(request):
-    print("Hey caching is working fine")
+    #print("Hey caching is working fine")
     #print("Home is called.")
-    return HttpResponse("<h1>This is my first view page</h1>" , status=500)
+    #return HttpResponse("<h1>This is my first view page</h1>")
     #return HttpResponseNotFound("<h1>The Http request hasnot been found</h1>"
+    #return render(request , "app1/home.html")
+    return redirect (login)
+
+def login(request):
+    return HttpResponse("<h1> This is my login page </h1>")
+   # return render(request, "login.html")
